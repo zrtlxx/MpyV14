52966478ad2bcbc3321d39c64dc55ddfe4b6b29d
zrtlxx <110093701+zrtlxx@users.noreply.github.com>
2022-08-18 18:51:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6281223084340'],
+  ['6281223084340'],
+  ['6281223084340', '✿͜͡ঔৣ͜͡🇮🇩⃢𐎈', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['✿͜͡ঔৣ͜͡🇮🇩⃢𐎈']]
+global.namabot = [['ZeroBotz']]
+global.ownerinsta = [['@zro?']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+  var sticker_author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
